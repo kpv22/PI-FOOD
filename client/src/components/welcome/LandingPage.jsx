945eb6d0710a670fda13a5f35caf78986fccc2d6
Kevin Picado <kvap224@gmail.com>
2022-12-21 00:45:07 -0600

    new landing finished

--- client/src/components/welcome/LandingPage.jsx
+++ client/src/components/welcome/LandingPage.jsx
@@ -6,17 +6,16 @@ import Video3 from "../../assets/Video3.mp4";
 export function Landing(props) {
   return (
     <div className={styles.Landing}>
-      <div className={styles.overlay}>
-        <video src={Video3} muted autoPlay loop />
-      </div>
+      <video src={Video3} muted autoPlay loop />
       <div className={styles.content}>
-        <h1 className={styles.title}>Hello, my name is Kevin.</h1>
-        <div className={styles.subtitle}>Welcome to my website!</div>
-        <Link className={styles.textDe} to="/home">
-          <button className={styles.btn}>PI-FOOD</button>{" "}
-        </Link>
+        <div className={styles.typewriter}>
+          <h1 className={styles.title}>Hello, my name is Kevin.</h1>
+          <div className={styles.subtitle}>Welcome to my website!</div>
+          <Link className={styles.textDe} to="/home">
+            <button className={styles.btn}>PI-FOOD</button>{" "}
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
-//
