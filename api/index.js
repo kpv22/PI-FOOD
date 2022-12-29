1ad69203a0493808b9ebc2a18020ea5b2db9f438
Kevin Picado <kvap224@gmail.com>
2022-12-29 14:49:37 -0600

    restructur

--- api/index.js
+++ api/index.js
@@ -17,12 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
 });
